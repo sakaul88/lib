76ff23b9bf42b9db2368e6b6fd261f200e7dcc72
Alan Byrne <alan.byrne@ie.ibm.com>
2019-05-27 21:18:02 +0100

    improve log message

--- baseutils/baseutils.py
+++ baseutils/baseutils.py
@@ -113,7 +113,7 @@ def exe_cmd(cmd, working_dir=None, obfuscate=None, stdin=None, env=None, log_lev
         if raise_exception:
             raise Exception('Error executing command: {cmd}. RC: {rc}'.format(cmd=obfus_cmd, rc=rc))
         elif log_level != logging.NOTSET:  # User may be trying to avoid logging potentially expected failures, polluting the log
-            logger.error('Error executing command: {cmd}. RC: {rc}'.format(cmd=obfus_cmd, rc=rc))
+            logger.info('Command returned RC {rc} but received instruction not to raise exception'.format(rc=rc))
     else:
         logger.info('Command successful. Returning output')
     return (rc, output)
