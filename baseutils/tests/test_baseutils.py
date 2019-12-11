bd0719a0c51b9d1012937c6e0d2ba6230ec1d5fd
Alan Byrne <alan.byrne@ie.ibm.com>
2019-12-11 12:41:43 +0000

    updates

--- baseutils/tests/test_baseutils.py
+++ baseutils/tests/test_baseutils.py
@@ -36,10 +36,10 @@ class TestUtils(unittest.TestCase):
 
     def test_discover_github_latest_patch_version(self):
         release_url = 'https://api.github.com/repos/kubernetes/kubernetes/releases'
-        version_not_passing_patch = baseutils.discover_github_latest_patch_release('1.12', release_url)
-        version_passing_patch = baseutils.discover_github_latest_patch_release('1.12.1', release_url)
+        version_not_passing_patch = baseutils.discover_github_latest_patch_release('1.16', release_url)
+        version_passing_patch = baseutils.discover_github_latest_patch_release('1.16.1', release_url)
         self.assertEqual(version_not_passing_patch, version_passing_patch)
-        self.assertTrue(version_not_passing_patch.startswith('v1.12.'))
+        self.assertTrue(version_not_passing_patch.startswith('v1.16.'))
 
     def test_exe_cmd(self):
         self.assertEqual((0, 'value\n'), baseutils.exe_cmd('echo value'))
