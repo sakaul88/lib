a1d002610b50c7a4ed248c243adfd1a561de6435
Michael Erickson <emike@ca.ibm.com>
2020-10-20 08:07:58 -0700

    fixed syntax

--- baseutils/baseutils.py
+++ baseutils/baseutils.py
@@ -303,7 +303,7 @@ def send_p2paas_slack(token, msg_title, msg_id='Unknown', msg_severity=None, clu
     job_name = os.environ.get('tower_job_template_name', 'Unknown')
     lines.append('AWX Template: {}'.format(job_name))
     job_id = os.environ.get('tower_job_id', 'Unknown')
-    lines.append('AWX Job: {}'.format(job_id)
+    lines.append('AWX Job: {}'.format(job_id))
 
     if msg_details is not None:
         lines.append('```{}```'.format(msg_details))
