2b6b3cb167f287ff598e177915bc3def02277156
Michael Erickson <emike@ca.ibm.com>
2020-10-20 07:50:00 -0700

    added more values to send_p2paas_slack

--- baseutils/baseutils.py
+++ baseutils/baseutils.py
@@ -300,6 +300,10 @@ def send_p2paas_slack(token, msg_title, msg_id='Unknown', msg_severity=None, clu
         lines.append('Severity: {}'.format(sev))
     if cluster is not None:
         lines.append('Cluster: {}'.format(cluster))
+    if 'tower_job_template_name' in os.environ:
+        lines.append('AWX Template: {}'.format(os.environ['tower_job_template_name']))
+    if 'tower_job_id' in os.environ:
+        lines.append('AWX Job: {}'.format(os.environ['tower_job_id']))
     if job is not None:
         lines.append('AWX Job: {}'.format(job))
     if msg_details is not None:
